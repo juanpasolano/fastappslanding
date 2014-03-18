a0c84f7c28a470d0f3f6c279c54ecb1dce48f528
juanpasolano <juanpa_solano@hotmail.com>
2014-03-18 09:01:31 -0500

    markdown, psd, others

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,17 @@
 
+"use strict";
 var app = angular.module('theapp', []);
 
 app.controller('MainCtrl', function($scope) {
   $scope.welcome = 'Hello world! This is HTML5 Boilerplate.';
+});
+
+
+$(document).ready(function(){
+	var element = $('.markdown');
+	var converter = new Showdown.converter();
+	var html = converter.makeHtml(element.text());
+	element.html(html);
+	SyntaxHighlighter.all();
+  // $('pre').each(function(i, e) {hljs.highlightBlock(e)});
 });
