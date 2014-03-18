aa6cc3b427110c7f79b2396c95a52b4cf6edcc1b
juanpasolano <juanpa_solano@hotmail.com>
2014-03-18 00:04:58 -0500

    demo

--- www/demo/js/app/filters/generalFilters.js
+++
@@ -1,9 +0,0 @@
-
-/*----------------
- /*FILTERS:
- /*---------------*/
-app.filter('upperCase', function(){
-	return function(text){
-		return text.toUpperCase();
-	};
-});
--- www/demo/js/dist/built.js
+++ www/demo/js/dist/built.js
@@ -73,6 +73,285 @@ app.factory('ConfigFactory', [
 	}
 ]);
 /*
+* CalendarCtrl.js
+*/
+
+app.controller('CalendarCtrl', ['$scope', '$rootScope', '$location', 'ConfigFactory',
+	function($scope, $rootScope, $location, ConfigFactory){
+		ConfigFactory.title = 'Calendar';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = true;
+		ConfigFactory.hasSideNavigation = true;
+		$scope.config = ConfigFactory;
+
+		$scope.events = [
+			{ date: '2014-01-09', title: 'Lorem ipsum dolor sit.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit 2.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit 3.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit 4.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit 5.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Lorem ipsum dolor sit 6.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-09', title: 'Quickly embrace high standards in.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-12', title: 'Quickly deliver state of.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-18', title: 'Compellingly re-engineer client.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-02-22', title: 'Appropriately expedite', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-03-11', title: 'Lorem ipsum dolor sit.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-03-13', title: 'Quickly embrace high standards in.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-03-13', title: 'Quickly deliver state of.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-03-23', title: 'Compellingly re-engineer client.', url: 'http://github.com/kylestetz/CLNDR' },
+			{ date: '2014-03-26', title: 'Appropriately expedite', url: 'http://github.com/kylestetz/CLNDR' }
+		];
+		$scope.calendarOptions = {
+			clickEvents: {
+				click: function(target) {
+					if(target.events.length > 0){
+						$rootScope.$emit('makeModal', {
+							options:{
+								template:'partials/modals/calendarModal.html',
+								cancelText :'Yep',
+								acceptText: 'Ok, go.',
+								title: 'Events on '+ target.date._i,
+								data: target.events
+							}
+						});
+					}
+				},
+				onMonthChange: function(month) {
+					console.log('you just went to ' + month.format('MMMM, YYYY'));
+				}
+			},
+			doneRendering: function() {
+				console.log('this would be a fine place to attach custom event handlers.');
+			}
+		};
+
+
+		$scope.addEvent = function(){
+			$scope.events.push($scope.newEvent);
+			$scope.newEvent = {};
+		};
+
+		$scope.removeEvent = function(){
+			$scope.events.splice($scope.events.length-1,1);
+		};
+	}
+]);
+/*
+* ChartsjsCtrl.js
+*/
+
+app.controller('ChartsjsCtrl', ['$scope', '$timeout', 'ConfigFactory',
+	function($scope, $timeout, ConfigFactory){
+		ConfigFactory.title = 'Chart.js';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = false;
+		ConfigFactory.hasSideNavigation = true;
+		$scope.config = ConfigFactory;
+
+
+		// var data = [
+		// {name : 'Colico', value: 5},
+		// {name : 'Dolor cabeza', value: 6},
+		// {name : 'Dolor espalda', value: 8},
+		// {name : 'Nauseas', value: 6},
+		// ];
+
+		var ctx = $("#myChart").get(0).getContext("2d");
+
+
+
+		// var data = {
+		// 	labels : ["January","February","March","April","May","June","July"],
+		// 	datasets : [
+		// 		{
+		// 			fillColor : "rgba(220,220,220,0.5)",
+		// 			strokeColor : "rgba(220,220,220,1)",
+		// 			pointColor : "rgba(220,220,220,1)",
+		// 			pointStrokeColor : "#fff",
+		// 			data : [65,59,90,81,56,55,40]
+		// 		},
+		// 		{
+		// 			fillColor : "rgba(151,187,205,0.5)",
+		// 			strokeColor : "rgba(151,187,205,1)",
+		// 			pointColor : "rgba(151,187,205,1)",
+		// 			pointStrokeColor : "#fff",
+		// 			data : [28,48,40,19,96,27,100]
+		// 		}
+		// 	]
+		// };
+		// var myNewChart = new Chart(ctx).Line(data);
+
+		var options = {
+
+	//Boolean - If we show the scale above the chart data
+	scaleOverlay : false,
+
+	//Boolean - If we want to override with a hard coded scale
+	scaleOverride : true,
+
+	//** Required if scaleOverride is true **
+	//Number - The number of steps in a hard coded scale
+	scaleSteps : 10,
+	//Number - The value jump in the hard coded scale
+	scaleStepWidth : 1,
+	//Number - The scale starting value
+	scaleStartValue : 0,
+
+	//String - Colour of the scale line
+	scaleLineColor : "rgba(0,0,0,.1)",
+
+	//Number - Pixel width of the scale line
+	scaleLineWidth : 1,
+
+	//Boolean - Whether to show labels on the scale
+	scaleShowLabels : true,
+
+	//Interpolated JS string - can access value
+	scaleLabel : "<%=value%>",
+
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily : "'Arial'",
+
+	//Number - Scale label font size in pixels
+	scaleFontSize : 12,
+
+	//String - Scale label font weight style
+	scaleFontStyle : "normal",
+
+	//String - Scale label font colour
+	scaleFontColor : "#666",
+
+	///Boolean - Whether grid lines are shown across the chart
+	scaleShowGridLines : true,
+
+	//String - Colour of the grid lines
+	scaleGridLineColor : "rgba(0,0,0,.05)",
+
+	//Number - Width of the grid lines
+	scaleGridLineWidth : 1,
+
+	//Boolean - If there is a stroke on each bar
+	barShowStroke : true,
+
+	//Number - Pixel width of the bar stroke
+	barStrokeWidth : 2,
+
+	//Number - Spacing between each of the X value sets
+	barValueSpacing : 5,
+
+	//Number - Spacing between data sets within X values
+	barDatasetSpacing : 1,
+
+	//Boolean - Whether to animate the chart
+	animation : true,
+
+	//Number - Number of animation steps
+	animationSteps : 60,
+
+	//String - Animation easing effect
+	animationEasing : "easeOutQuart",
+
+	//Function - Fires when the animation is complete
+	onAnimationComplete : null
+};
+		var data = {
+			labels : ["Cólico","Dolor\nCabeza","Dolor Espalda","Nauseas"],
+			datasets : [
+				{
+					fillColor : "rgba(151,187,205,0.5)",
+					strokeColor : "rgba(151,187,205,1)",
+					data : [6,2,3,5]
+				}
+			]
+		};
+		var myNewChart = new Chart(ctx).Line(data, options);
+	}
+]);
+/*
+* D3ChartsCtrl.js
+*/
+
+app.controller('D3ChartsCtrl', ['$scope', '$timeout', 'ConfigFactory',
+	function($scope, $timeout, ConfigFactory){
+		ConfigFactory.title = 'd3 Charts';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = false;
+		ConfigFactory.hasSideNavigation = true;
+		$scope.config = ConfigFactory;
+
+
+		var data = [
+		{name : 'Colico', value: 5},
+		{name : 'Dolor cabeza', value: 6},
+		{name : 'Dolor espalda', value: 8},
+		{name : 'Nauseas', value: 6},
+		];
+		var d3obj = d3.select(".d3-chart-bar")
+			.selectAll("div")
+			.data(data);
+
+		var barRowObj = d3obj.enter().append("div").attr('class', 'row bar-row');
+
+		barRowObj.append('div').attr('class', 'column small-2 tar bar-text')
+			.text(function(d) { return d.name; });
+
+		var barbarObj = barRowObj.append('div').attr('class', 'column small-10')
+			.append('div').attr('class', ' bar-bar')
+			.text(function(d) { return d.value; });
+
+		$timeout(function(){
+			barbarObj.style("width", function(d) { return d.value *10+"%"; });
+		}, 300);
+	}
+]);
+/*
+* DetailCtrl.js
+*/
+
+app.controller('DetailCtrl', ['$scope', '$routeParams', '$location', 'ConfigFactory', 'MusicService',
+	function($scope, $routeParams, $location, ConfigFactory, MusicService){
+		ConfigFactory.title = $routeParams.id;
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = false;
+		ConfigFactory.hasSideNavigation = true;
+		ConfigFactory.hasBackButton = true;
+		ConfigFactory.hasRightButton = true;
+		$scope.config = ConfigFactory;
+
+		$scope.itemId = $routeParams.id;
+
+		var itemSuccess = function(data, status){
+			$scope.item = data.album;
+			console.log(status);
+		};
+
+		MusicService.getDetail($routeParams.id).success(itemSuccess);
+	}
+]);
+/*
+* FormsCtrl.js lalala
+*/
+app.controller('FormsCtrl', ['$scope', '$location', 'ConfigFactory',
+	function($scope, $location, ConfigFactory){
+		ConfigFactory.title = 'Forms';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = true;
+		ConfigFactory.hasSideNavigation = true;
+		$scope.config = ConfigFactory;
+
+		// $scope.check1 = false;
+
+		$scope.formInfo = {};
+		$scope.formInfo.check1 = true;
+
+	}
+]);
+
+
+
+
+/*
 * HomeCtrl.js
 */
 app.controller('HomeCtrl', ['$scope', '$rootScope', '$timeout', 'ConfigFactory',
@@ -116,6 +395,33 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$timeout', 'ConfigFactory',
 	}
 ]);
 /*
+* ListViewCtrl.js
+*/
+app.controller('ListViewCtrl', ['$scope', '$location', 'ConfigFactory', 'MusicService',
+	function($scope, $location, ConfigFactory, MusicService){
+		ConfigFactory.title = 'List View';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = true;
+		ConfigFactory.hasSideNavigation = true;
+		ConfigFactory.hasBackButton = false;
+		ConfigFactory.hasRightButton = false;
+		ConfigFactory.rightButtonFn = function(ev){
+			console.log(ev);
+		};
+
+		var itemsSuccess = function(data, status){
+			$scope.items = data.results.albummatches.album;
+			console.log(status);
+		};
+		MusicService.getStores().success(itemsSuccess);
+
+		$scope.getDetails = function(item){
+			$location.path('detailDefault/'+item.name);
+			console.log(item);
+		};
+	}
+]);
+/*
 * HomeCtrl.js
 */
 app.controller('LoginCtrl',[ '$scope', '$rootScope', '$location', 'ConfigFactory',
@@ -134,6 +440,38 @@ app.controller('LoginCtrl',[ '$scope', '$rootScope', '$location', 'ConfigFactory
 	}
 ]);
 /*
+ * MapCtrl.js
+ */
+
+app.controller('MapCtrl', ['$scope', '$log', 'ConfigFactory', 'StoresModel',
+	function($scope, $log, ConfigFactory, StoresModel){
+		ConfigFactory.title = 'Map demo';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = true;
+		ConfigFactory.hasSideNavigation = true;
+
+		$scope.mapOptions = {
+			zoom:13,
+			center :{
+				lat:4.582226749273246,
+				lng: -74.09687547013164
+			}
+		};
+		$scope.branches = '';
+		$scope.markerClick =  function(marker){
+			alert('marker clicked', marker);
+		};
+
+
+		var storesSuccess = function(data, status){
+			console.log(data);
+			$scope.branches = data[3].branches;
+		};
+		StoresModel.getStores().success(storesSuccess);
+	}
+]);
+
+/*
 * RecoverCtrl.js
 */
 app.controller('RecoverCtrl',[ '$scope', '$rootScope', '$location', 'ConfigFactory',
@@ -149,6 +487,34 @@ app.controller('RecoverCtrl',[ '$scope', '$rootScope', '$location', 'ConfigFacto
 			$scope.mailsent = !$scope.mailsent;
 		};
 	}
+]);
+/*
+* SwiperCtrl.js
+*/
+
+app.controller('SwiperCtrl', [ '$scope', '$location', 'ConfigFactory',
+
+	function($scope, $location, ConfigFactory){
+		ConfigFactory.title = 'Swiper plugin';
+		ConfigFactory.hasHeader = true;
+		ConfigFactory.hasFooter = false;
+		ConfigFactory.hasSideNavigation = true;
+		$scope.config = ConfigFactory;
+
+
+		$scope.swithcExample = function(example){
+			$scope.exampleDisplayed = example;
+		};
+
+		$scope.swiperOpts1 ={
+			pagination: '.swiper-pagination',
+			cssWidthAndHeight: false,
+			loop:true,
+			grabCursor: true,
+			paginationClickable: false
+		};
+	}
+
 ]);
 /*
 * CalendarDirective.js
@@ -715,19 +1081,51 @@ app.config([ '$routeProvider',
 	function($routeProvider){
 		$routeProvider
 		.when('/login', {
-			templateUrl: 'partials/login.html',
+			templateUrl: 'partials/login/login.html',
 			controller: 'LoginCtrl'
 		})
 		.when('/login/recover', {
-			templateUrl: 'partials/recover.html',
+			templateUrl: 'partials/login/recover.html',
 			controller: 'RecoverCtrl'
 		})
 		.when('/home', {
-			templateUrl: 'partials/home.html',
+			templateUrl: 'partials/home/home.html',
 			controller: 'HomeCtrl'
 		})
+		.when('/forms', {
+			templateUrl: 'partials/forms/forms.html',
+			controller: 'FormsCtrl'
+		})
+		.when('/listview', {
+			templateUrl: 'partials/listview/listview.html',
+			controller: 'ListViewCtrl'
+		})
+		.when('/detailDefault/:id', {
+			templateUrl: 'partials/details/detailDefault.html',
+			controller: 'DetailCtrl'
+		})
+		.when('/calendar', {
+			templateUrl: 'partials/calendar/calendar.html',
+			controller: 'CalendarCtrl'
+		})
+		.when('/swiper', {
+			templateUrl: 'partials/swiper/swiper.html',
+			controller: 'SwiperCtrl'
+		})
+		.when('/map', {
+			templateUrl: 'partials/map/map.html',
+			controller: 'MapCtrl'
+		})
+		.when('/chartsjs', {
+			templateUrl: 'partials/charts/chartsjs.html',
+			controller: 'ChartsjsCtrl'
+		})
+		.when('/d3Charts', {
+			templateUrl: 'partials/charts/d3Charts.html',
+			controller: 'D3ChartsCtrl'
+		})
 		.otherwise({
-			redirectTo:'/login'
+			redirectTo:'/home'
 		});
 	}
 ]);
